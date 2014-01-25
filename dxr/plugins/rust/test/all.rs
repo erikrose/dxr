21d6235410eabba2b99ce8d57bd57d8123f2ad97
Nick Cameron <ncameron@mozilla.com>
2014-01-25 16:24:01 +1300

    Rust support pt2

--- dxr/plugins/rust/test/all.rs
+++ dxr/plugins/rust/test/all.rs
@@ -1,27 +1,22 @@
-#![ crate_id = "test" ]
-#![feature(struct_variant)]
-#![feature(macro_rules)]
+#[ crate_id = "all" ];
+#[feature(struct_variant)];
+#[feature(macro_rules)];
+
+// Test everything! Everything that only requires one crate at least.
+// TODO split this up into more minimal tests.
 
 extern crate num;
-extern crate collections;
 // A simple rust project
 
 //extern crate krate2;
 extern crate myflate = "flate";
 
 use num::bigint::BigInt;
-use collections::{HashMap,HashSet};
-use std::cell::RefCell;
 
 use msalias = sub::sub2;
 use sub::sub2;
-use sub_struct = sub::sub2::nested_struct;
 use std::io::stdio::println;
 use std::num::cast;
-use std::num::{from_int,from_i8};
-
-use std::mem::size_of;
-use std::raw::Vec;
 
 static uni: &'static str = "Les Miséééééééérables";
 static yy: uint = 25u;
@@ -29,10 +24,6 @@ static yy: uint = 25u;
 static bob: Option<num::bigint::BigInt> = None;
 
 // buglink test - see issue #1337.
-
-fn test_aslias() {
-    let s = sub_struct{ field2: 45u32, };
-}
 
 mod sub {
     pub mod sub2 {
@@ -48,7 +39,7 @@ mod sub {
         }
 
         pub struct nested_struct {
-            pub field2: u32,
+            field2: u32,
         }
 
         pub enum nested_enum {
@@ -100,6 +91,7 @@ impl SomeTrait for some_fields {
 }
 
 impl SuperTrait for some_fields {
+
 }
 
 impl some_fields {
@@ -110,14 +102,6 @@ impl some_fields {
     fn stat2(x: &some_fields) -> u32 {
         42
     }  
-
-    fn align_to<T>(&mut self) {
-        
-    }
-
-    fn test(&mut self) {
-        self.align_to::<bool>();
-    }
 }
 
 impl SuperTrait for nofields {
@@ -135,9 +119,9 @@ impl SubTrait for nofields {
     }
 }
 
-impl SuperTrait for (Box<nofields>, Box<some_fields>) {}
+impl SuperTrait for (~nofields, ~some_fields) {}
 
-type MyType = Box<some_fields>;
+type MyType = ~some_fields;
 
 fn f_with_params<T: SomeTrait>(x: &T) {
     x.Method(41);
@@ -158,8 +142,7 @@ enum SomeOtherEnum {
 
 enum SomeStructEnum {
     EnumStruct{a:int, b:int},
-    EnumStruct2{f1:MyType, f2:MyType},
-    EnumStruct3{f1:MyType, f2:MyType, f3:SomeEnum}
+    EnumStruct2{f1:MyType, f2:MyType}
 }
 
 fn matchSomeEnum(val: SomeEnum) {
@@ -175,15 +158,6 @@ fn matchSomeStructEnum(se: SomeStructEnum) {
     match se {
         EnumStruct{a:a, ..} => println(a.to_str()),
         EnumStruct2{f1:f1, f2:f_2} => println(f_2.field1.to_str()),
-        EnumStruct3{f1, ..} => println(f1.field1.to_str()),
-    }
-}
-fn matchSomeStructEnum2(se: SomeStructEnum) {
-    match se {
-        EnumStruct{a: ref aaa, ..} => println(aaa.to_str()),
-        EnumStruct2{f1, f2: f2} => println(f1.field1.to_str()),
-        EnumStruct3{f1, f3: Ints(_, _), f2} => println(f1.field1.to_str()),
-        _ => {},
     }
 }
 
@@ -194,7 +168,7 @@ fn matchSomeOtherEnum(val: SomeOtherEnum) {
     }
 }
 
-fn hello<X: SomeTrait>((z, a) : (u32, ~str), ex: X) {
+fn hello((z, a) : (u32, ~str)) {
     SameDir2::hello(43);
 
     println(yy.to_str());
@@ -203,21 +177,19 @@ fn hello<X: SomeTrait>((z, a) : (u32, ~str), ex: X) {
     println(z.to_str());
     let x: u32 = x;
     println(x.to_str());
-    let x = "hello".to_owned();
+    let x = ~"hello";
     println(x);
 
     let x = 32.0f32;
     let _ = (x + ((x * x) + 1.0).sqrt()).ln();
 
-    let s: Box<SomeTrait> = box some_fields {field1: 43};
-    let s2: Box<some_fields> =  box some_fields {field1: 43};
-    let s3 = box nofields;
+    let s: ~SomeTrait = ~some_fields {field1: 43};
+    let s2: ~some_fields = ~some_fields {field1: 43};
+    let s3 = ~nofields;
 
     s.Method(43);
     s3.Method(43);
     s2.Method(43);
-
-    ex.prov(43);
 
     let y: u32 = 56;
     // static method on struct
@@ -228,11 +200,8 @@ fn hello<X: SomeTrait>((z, a) : (u32, ~str), ex: X) {
     // trait static method, calls default
     let r = SomeTrait::stat2(s3);
 
-    let s4 = s3 as Box<SubTrait>;
+    let s4 = s3 as ~SubTrait;
     s4.Method(43);
-
-    s4.provided_method();
-    s2.prov(45);
 
     let closure = |x: u32, s: &SomeTrait| {
         s.Method(23);
@@ -242,18 +211,8 @@ fn hello<X: SomeTrait>((z, a) : (u32, ~str), ex: X) {
     let z = closure(10, s);
 }
 
-// test kind bounds on trait objects
-fn kind_bounds(_: Box<SuperTrait>:Send, _: Box<SuperTrait>:'static) -> Box<SuperTrait>: {
-    fail!();
-}
-
-pub struct blah {
-    used_link_args: RefCell<~[~str]>,
-}
-
 fn main() {
-    let s = box some_fields {field1: 43};
-    hello((43, "a".to_owned()), *s);
+    hello((43, ~"a"));
     sub::sub2::hello();
     sub2::sub3::hello();
 
@@ -261,7 +220,7 @@ fn main() {
     h();
 
     // utf8 chars
-    let ut = "Les Miséééééééérables";
+    let _ = "Les Miséééééééérables";
 
     // For some reason, this pattern of macro_rules foiled our generated code
     // avoiding strategy.
@@ -272,27 +231,25 @@ fn main() {
     ));
     let vs = variable_str!(32);
 
-    let mut candidates: RefCell<HashMap<&'static str, &'static str>> = RefCell::new(HashMap::new());
-    let _ = blah {
-        used_link_args: RefCell::new(box []),
-    };
     let s1 = nofields;
     let s2 = some_fields{ field1: 55};
     let s3: some_fields = some_fields{ field1: 55};
     let s4: msalias::nested_struct = sub::sub2::nested_struct{ field2: 55};
     let s4: msalias::nested_struct = sub2::nested_struct{ field2: 55};
     println(s2.field1.to_str());
-    let s5: MyType = box some_fields{ field1: 55};
-    let s = SameDir::SameStruct{name: "Bob".to_owned()};
-    let s = SubDir::SubStruct{name:"Bob".to_owned()};
-    let s6: SomeEnum = MyTypes(box s2, s5);
-    let s7: SomeEnum = Strings("one".to_owned(), "two".to_owned(), "three".to_owned());
+    let s5: MyType = ~some_fields{ field1: 55};
+    let s = SameDir::SameStruct{name:~"Bob"};
+    let s = SubDir::SubStruct{name:~"Bob"};
+    let s6: SomeEnum = MyTypes(~s2, s5);
+    let s7: SomeEnum = Strings(~"one",~"two",~"three");
     matchSomeEnum(s6);
     matchSomeEnum(s7);
     let s8: SomeOtherEnum = SomeConst2;
     matchSomeOtherEnum(s8);
-    let s9: SomeStructEnum = EnumStruct2{f1: box some_fields{field1:10}, f2: box s2};
+    let s9: SomeStructEnum = EnumStruct2{f1: ~some_fields{field1:10}, f2: ~s2};
     matchSomeStructEnum(s9);
 }
 
+
+//pub fn main() {}
 
